18b8fbe0fc5dd367ddc763cec02e2eaa25568f1d
Markus <endzeit@gmail.com>
2018-05-17 15:01:36 +0200

    Add extension examples

--- examples/_fixtures/sample-extension/background.js
+++ examples/_fixtures/sample-extension/background.js
@@ -1,3 +1,10 @@
 console.info("Hello from background!", chrome.runtime.id)
 
 window.addSeven = (number) => number + 7
+
+const delay = ms => new Promise(_ => setTimeout(_, ms));
+
+window.addNineAsync = async (number) => {
+  await delay(1000)
+  return number + 9
+}
--- examples/extensions.js
+++ examples/extensions.js
@@ -20,14 +20,42 @@ const EXT_PATH = `${__dirname}/_fixtures/sample-extension`;
 
   // By using .evaluate() we can run code executed in the context of the extensions background page
   const res1 = await sampleExtension.evaluate(`window.addSeven(6)`)
-  console.debug("Result of window.addSeven(6)", res1)
+  console.debug("Result of window.addSeven(6):", res1)
+
+  // Evaluate async function
+  const res2 = await sampleExtension.evaluate(`window.addNineAsync(6)`)
+  console.debug("Result of window.addNineAsync(6):", res2)
 
   // We have access to chrome APIs as well
-  const res2 = await sampleExtension.evaluate(`chrome.runtime.getManifest()`)
-  console.debug("Result of chrome.runtime.getManifest()", res2)
+  const res3 = await sampleExtension.evaluate(`chrome.runtime.getManifest()`)
+  console.debug("Result of chrome.runtime.getManifest():", res3)
 
   // Console output will show up when inspecting the background page of the extension
   await sampleExtension.evaluate(`console.log("puppeteer-extra was here")`)
 
   await browser.close();
 })()
+
+/*
+Result of the above:
+
+Found sample extension { targetId: '4D9E700DA7F5BEA78B50FA4DC903BA18',
+  type: 'background_page',
+  title: 'Shiny Sample Extension',
+  url: 'chrome-extension://lekbicckbfmgelnaehjfmgeofigialpe/_generated_background_page.html',
+  attached: false,
+  browserContextId: '21560410BB34F1979816C4E2A8E96C4D',
+  evaluate: [AsyncFunction] }
+Result of window.addSeven(6): { result: { type: 'number', value: 13, description: '13' } }
+Result of window.addNineAsync(6): { result: { type: 'number', value: 15, description: '15' } }
+Result of chrome.runtime.getManifest(): { result:
+   { type: 'object',
+     value:
+      { background: [Object],
+        current_locale: 'en_US',
+        default_locale: 'en',
+        description: 'Just a minimal sample Extension',
+        manifest_version: 2,
+        name: 'Shiny Sample Extension',
+        version: '0.0.1' } } }
+*/
