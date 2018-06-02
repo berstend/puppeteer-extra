2fefa134bd3d12f4a1e9f7ed86f44443b38d1098
Markus <endzeit@gmail.com>
2018-06-02 16:14:51 +0200

    Add puppeteer-extra-plugin-devtools

---
+++ packages/puppeteer-extra-plugin-devtools/test/headless.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { test } = require('ava')
+
+test.beforeEach(t => {
+  // Make sure we work with pristine modules
+  delete require.cache[require.resolve('puppeteer-extra')]
+  delete require.cache[require.resolve('puppeteer-extra-plugin-devtools')]
+})
+
+test('will create a tunnel', async (t) => {
+  const puppeteer = require('puppeteer-extra')
+  const devtools = require('puppeteer-extra-plugin-devtools')()
+  puppeteer.use(devtools)
+  devtools.setAuthCredentials('bob', 'swordfish')
+
+  await puppeteer.launch().then(async browser => {
+    const tunnel = await devtools.createTunnel(browser)
+    t.true(tunnel.url.includes('https://devtools-tunnel-'))
+    t.true(tunnel.url.includes('.localtunnel.me'))
+    browser.close()
+  })
+  t.true(true)
+})
