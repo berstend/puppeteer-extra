948d50662d47e7291f2501f163dab73b66701ac5
berstend̡̲̫̹̠̖͚͓̔̄̓̐̄͛̀͘ <github@berstend.com>
2020-08-06 19:20:21 +0200

    fix(plugin-stealth): Harden navigator.vendor evasion (#276)

--- packages/puppeteer-extra-plugin-stealth/evasions/navigator.vendor/index.test.js
+++ packages/puppeteer-extra-plugin-stealth/evasions/navigator.vendor/index.test.js
@@ -21,3 +21,19 @@ test('stealth: navigator.vendor set to custom value', async t => {
   const vendor = await page.evaluate(() => navigator.vendor)
   t.true(vendor === 'Apple Computer, Inc.')
 })
+
+test('stealth: will not leak modifications', async t => {
+  const puppeteer = addExtra(vanillaPuppeteer).use(Plugin())
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+
+  const test1 = await page.evaluate(
+    () => Object.getOwnPropertyDescriptor(navigator, 'vendor') // Must be undefined if native
+  )
+  t.is(test1, undefined)
+
+  const test2 = await page.evaluate(
+    () => Object.getOwnPropertyNames(navigator) // Must be an empty array if native
+  )
+  t.deepEqual(test2, [])
+})
