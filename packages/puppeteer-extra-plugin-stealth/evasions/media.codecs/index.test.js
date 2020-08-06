98902dcaca696c4df3cf0d41e4f08affd47214bb
Markus <endzeit@gmail.com>
2020-08-06 08:44:25 +0200

    chore(plugin-stealth): Add leak tests for media.codecs

--- packages/puppeteer-extra-plugin-stealth/evasions/media.codecs/index.test.js
+++ packages/puppeteer-extra-plugin-stealth/evasions/media.codecs/index.test.js
@@ -4,6 +4,8 @@ const {
   getVanillaFingerPrint,
   getStealthFingerPrint
 } = require('../../test/util')
+const { vanillaPuppeteer, addExtra } = require('../../test/util')
+
 const Plugin = require('.')
 
 test('vanilla: doesnt support proprietary codecs', async t => {
@@ -16,6 +18,24 @@ test('vanilla: doesnt support proprietary codecs', async t => {
     m4a: '',
     aac: ''
   })
+})
+
+test('vanilla: will not have modifications', async t => {
+  const browser = await vanillaPuppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+
+  // https://datadome.co/bot-detection/client-side-detection-is-essential-for-bot-protection/
+  const test1 = await page.evaluate(() => {
+    const audioElt = document.createElement('audio')
+    return audioElt.canPlayType.toString()
+  })
+  t.is(test1, 'function canPlayType() { [native code] }')
+
+  const test2 = await page.evaluate(() => {
+    const audioElt = document.createElement('audio')
+    return audioElt.canPlayType.name
+  })
+  t.is(test2, 'canPlayType')
 })
 
 test('stealth: supports proprietary codecs', async t => {
@@ -32,4 +52,30 @@ test('stealth: supports proprietary codecs', async t => {
     m4a: 'maybe',
     aac: 'probably'
   })
+})
+
+test('stealth: will not leak modifications', async t => {
+  const puppeteer = addExtra(vanillaPuppeteer).use(Plugin())
+  const browser = await puppeteer.launch({ headless: true })
+  const page = await browser.newPage()
+
+  // https://datadome.co/bot-detection/client-side-detection-is-essential-for-bot-protection/
+  const test1 = await page.evaluate(() => {
+    const audioElt = document.createElement('audio')
+    return audioElt.canPlayType.toString()
+  })
+  t.is(test1, 'function canPlayType() { [native code] }')
+
+  const test2 = await page.evaluate(() => {
+    const audioElt = document.createElement('audio')
+    return audioElt.canPlayType.name
+  })
+  t.is(test2, 'canPlayType')
+
+  // Double check the plugin is active and spoofing e.g. the aac codec results
+  const test3 = await page.evaluate(() => {
+    const audioElt = document.createElement('audio')
+    return audioElt.canPlayType('audio/aac')
+  })
+  t.is(test3, 'probably') // empty in Chromium without stealth plugin
 })
