8f3ffbfcb09f0781c0f320750b8de1f05c8c6fd9
berstend̡̲̫̹̠̖͚͓̔̄̓̐̄͛̀͘ <github@berstend.com>
2020-08-18 17:32:43 +0200

    chore(extract-stealth-evasions): Add bin to package.json (#294)
    Co-authored-by: Niek van der Maas <mail@niekvandermaas.nl>

--- packages/extract-stealth-evasions/index.js
+++ packages/extract-stealth-evasions/index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer-extra')
-const stealth = require('puppeteer-extra-plugin-stealth')();
-const { minify } = require("terser");
+const stealth = require('puppeteer-extra-plugin-stealth')()
+const { minify } = require('terser')
 const argv = require('yargs')
   .usage('Usage: $0 [options]')
   .alias('e', 'exclude')
@@ -10,11 +10,10 @@ const argv = require('yargs')
   .alias('l', 'list')
   .describe('l', 'List available evasions')
   .help('h')
-  .alias('h', 'help')
-  .argv;
-const fs = require("fs");
+  .alias('h', 'help').argv
+const fs = require('fs')
 
-const file = 'stealth.min.js';
+const file = 'stealth.min.js'
 
 if (argv.exclude) {
   if (typeof argv.exclude === 'string') {
@@ -22,50 +21,57 @@ if (argv.exclude) {
   } else {
     argv.exclude.forEach(e => {
       stealth.enabledEvasions.delete(e)
-    });
+    })
   }
 } else if (argv.include) {
   if (typeof argv.include === 'string') {
     stealth.enabledEvasions = [argv.include]
   } else {
-    stealth.enabledEvasions = [];
+    stealth.enabledEvasions = []
     argv.include.forEach(e => {
       stealth.enabledEvasions.push(e)
-    });
+    })
   }
 } else if (argv.list) {
-  console.log('Available evasions:', [...stealth.availableEvasions].join(', '));
-  process.exit(0);
+  console.log('Available evasions:', [...stealth.availableEvasions].join(', '))
+  process.exit(0)
 }
 
-let scripts = '';
+let scripts = ''
 
 puppeteer
   .use(stealth)
   .launch({
-    headless: true,
+    headless: true
   })
   .then(async browser => {
     // Patch evaluateOnNewDocument()
-    const page = (await browser.pages()).find(Boolean);
-    page.__proto__.evaluateOnNewDocument = patchEval;
-    page.__proto__.evaluate = patchEval;
+    const page = (await browser.pages()).find(Boolean)
+    page.__proto__.evaluateOnNewDocument = patchEval // eslint-disable-line no-proto
+    page.__proto__.evaluate = patchEval // eslint-disable-line no-proto
 
-    await (await browser.newPage()).goto('about:blank');
-    await browser.close();
+    await (await browser.newPage()).goto('about:blank')
+    await browser.close()
 
-    fs.writeFile(file, (await minify(scripts, { toplevel: true })).code, (err) => {
-      if (err) throw err;
-      console.log(`File ${file} written!`);
-      console.log('Included evasions: ', [...stealth.enabledEvasions].join(', '));
-    });
-  });
+    fs.writeFile(
+      file,
+      (await minify(scripts, { toplevel: true })).code,
+      err => {
+        if (err) throw err
+        console.log(`File ${file} written!`)
+        console.log(
+          'Included evasions: ',
+          [...stealth.enabledEvasions].join(', ')
+        )
+      }
+    )
+  })
 
 function patchEval(f, args) {
   // Check if there are options supplied
   if (typeof args != 'undefined') {
-    scripts += '(' + f.toString() + ')(' + JSON.stringify(args) + ');\n';
+    scripts += '(' + f.toString() + ')(' + JSON.stringify(args) + ');\n'
   } else {
-    scripts += '(' + f.toString() + ')();\n';
+    scripts += '(' + f.toString() + ')();\n'
   }
 }
