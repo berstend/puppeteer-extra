a6790ed6ec990bc9fd9511bb08e4a959c7efaf5a
Markus <endzeit@gmail.com>
2020-09-30 10:00:41 +0200

    chore(plugin-anonymize-ua): Switch tests to use automation-extra

--- packages/puppeteer-extra-plugin-anonymize-ua/test/headless.js
+++ packages/puppeteer-extra-plugin-anonymize-ua/test/headless.js
@@ -2,43 +2,14 @@
 
 const test = require('ava')
 
-const PUPPETEER_ARGS = ['--no-sandbox', '--disable-setuid-sandbox']
+const { wrap } = require('testing-tools')
 
-test.beforeEach(t => {
-  // Make sure we work with pristine modules
-  delete require.cache[require.resolve('puppeteer-extra')]
-  delete require.cache[require.resolve('puppeteer-extra-plugin-anonymize-ua')]
-})
+wrap(test)('pptr:chromium')(
+  'will remove headless from the user-agent',
+  async (t, driver) => {
+    const plugin = require('puppeteer-extra-plugin-anonymize-ua')()
+    const { browser, page } = await driver.getPageWithPlugin(plugin)
 
-test('will remove headless from the user-agent', async t => {
-  const puppeteer = require('puppeteer-extra')
-  const AnonymizeUA = require('puppeteer-extra-plugin-anonymize-ua')()
-  puppeteer.use(AnonymizeUA)
-
-  const browser = await puppeteer.launch({ args: PUPPETEER_ARGS })
-  const page = await browser.newPage()
-  await page.goto('https://httpbin.org/headers', {
-    waitUntil: 'domcontentloaded'
-  })
-
-  const content = await page.content()
-  t.true(content.includes('Windows NT 10.0'))
-  t.true(!content.includes('HeadlessChrome'))
-
-  await browser.close()
-  t.true(true)
-})
-
-test('will remove headless from the user-agent in incognito page', async t => {
-  const puppeteer = require('puppeteer-extra')
-  puppeteer.use(require('puppeteer-extra-plugin-anonymize-ua')())
-
-  const browser = await puppeteer.launch({ args: PUPPETEER_ARGS })
-
-  // Requires puppeteer@next currrently
-  if (browser.createIncognitoBrowserContext) {
-    const context = await browser.createIncognitoBrowserContext()
-    const page = await context.newPage()
     await page.goto('https://httpbin.org/headers', {
       waitUntil: 'domcontentloaded'
     })
@@ -46,32 +17,55 @@ test('will remove headless from the user-agent in incognito page', async t => {
     const content = await page.content()
     t.true(content.includes('Windows NT 10.0'))
     t.true(!content.includes('HeadlessChrome'))
+
+    await browser.close()
+    t.true(true)
   }
+)
 
-  await browser.close()
-  t.true(true)
-})
+wrap(test)('pptr:chromium')(
+  'will remove headless from the user-agent in incognito page',
+  async (t, driver) => {
+    const plugin = require('puppeteer-extra-plugin-anonymize-ua')()
+    const browser = await driver.getBrowserWithPlugin(plugin)
 
-test('will use a custom fn to modify the user-agent', async t => {
-  const puppeteer = require('puppeteer-extra')
-  puppeteer.use(
-    require('puppeteer-extra-plugin-anonymize-ua')({
+    // Requires puppeteer@next currrently
+    if (browser.createIncognitoBrowserContext) {
+      const context = await browser.createIncognitoBrowserContext()
+      const page = await context.newPage()
+      await page.goto('https://httpbin.org/headers', {
+        waitUntil: 'domcontentloaded'
+      })
+
+      const content = await page.content()
+      t.true(content.includes('Windows NT 10.0'))
+      t.true(!content.includes('HeadlessChrome'))
+    }
+
+    await browser.close()
+    t.true(true)
+  }
+)
+
+wrap(test)('pptr:chromium')(
+  'will use a custom fn to modify the user-agent',
+  async (t, driver) => {
+    const plugin = require('puppeteer-extra-plugin-anonymize-ua')({
       customFn: ua => 'MyCoolAgent/' + ua.replace('Chrome', 'Beer')
     })
-  )
+    const { browser, page } = await driver.getPageWithPlugin(plugin)
 
-  const browser = await puppeteer.launch({ args: PUPPETEER_ARGS })
-  const page = await browser.newPage()
-  await page.goto('https://httpbin.org/headers', {
-    waitUntil: 'domcontentloaded'
-  })
+    await page.goto('https://httpbin.org/headers', {
+      waitUntil: 'domcontentloaded'
+    })
 
-  const content = await page.content()
-  t.true(content.includes('Windows NT 10.0'))
-  t.true(!content.includes('HeadlessChrome'))
-  t.true(content.includes('MyCoolAgent/Mozilla'))
-  t.true(content.includes('Beer/'))
+    const content = await page.content()
+    t.true(content.includes('Windows NT 10.0'))
+    t.true(!content.includes('HeadlessChrome'))
+    t.true(content.includes('MyCoolAgent/Mozilla'))
+    t.true(content.includes('Beer/'))
 
-  await browser.close()
-  t.true(true)
-})
+    await browser.close()
+    t.true(true)
+  }
+)
--- packages/testing-tools/src/index.ts
+++ packages/testing-tools/src/index.ts
@@ -7,33 +7,84 @@ export * as playwright from 'playwright'
 import { addExtra } from 'automation-extra'
 
 export type Browsers = 'chromium' | 'firefox' | 'webkit'
-export type Drivers = 'playwright' | 'puppeteer'
+export type DriverShortNames = 'pw' | 'pptr'
 
-export const getEnv = () => {
-  const DRIVER = (process.env.DRIVER || 'puppeteer') as Drivers
-  const BROWSER = (process.env.BROWSER || 'chromium') as Browsers
-  return { DRIVER, BROWSER }
+const getHelper = (driver: DriverShortNames, browser: Browsers) => {
+  const getLauncher = () => {
+    console.log({ driver, browser })
+    if (driver === 'pptr') {
+      return puppeteer
+    }
+    if (driver === 'pw') {
+      return playwright[browser]
+    }
+    throw new Error(`Unsupported driver: "${driver}"`)
+  }
+
+  const getLauncherWithExtra = () => addExtra(getLauncher())
+
+  const getBrowserWithPlugin = async (plugin: any) => {
+    const launcher = getLauncherWithExtra()
+    launcher.use(plugin)
+    const browser = await launcher.launch()
+    return browser
+  }
+
+  const getPageWithPlugin = async (plugin: any) => {
+    const browser = await getBrowserWithPlugin(plugin)
+    const page = await browser.newPage()
+    return { page, browser }
+  }
+
+  return {
+    driverName: driver,
+    browserName: browser,
+
+    getLauncher,
+    getLauncherWithExtra,
+    getBrowserWithPlugin,
+    getPageWithPlugin
+  }
 }
 
-export const getLauncher = () => {
-  const { DRIVER, BROWSER } = getEnv()
-  console.log({ DRIVER, BROWSER })
-  if (DRIVER === 'puppeteer') {
-    return puppeteer
-  }
-  if (DRIVER === 'playwright') {
-    return playwright[BROWSER]
-  }
-  throw new Error(`Unsupported driver: "${DRIVER}"`)
+export const driver = (
+  driver: DriverShortNames = 'pptr',
+  browser: Browsers = 'chromium'
+) => getHelper(driver, browser)
+
+export const makeTest = (testFn: any) => {
+  testFn.title = (title = '', driverName = '') => `${driverName}: ${title}`
+  return testFn
 }
 
-export const getLauncherWithExtra = () => addExtra(getLauncher())
+export const withDriver = (
+  ava: any,
+  title: string,
+  testFn: any,
+  driverName: string
+) => {
+  testFn.title = (title = '', driverName = '') => `${driverName}: ${title}`
+  ava(title, testFn, driverName)
+}
 
-export const getPageWithPlugin = async (plugin: any) => {
-  const launcher = getLauncherWithExtra()
-  launcher.use(plugin)
+export type TestCasesPlaywright =
+  | 'pw:all'
+  | 'pw:chromium'
+  | 'pw:firefox'
+  | 'pw:webkit'
+export type TestCasesPuppeteer = 'pptr:all' | 'pptr:chromium' | 'pptr:firefox'
 
-  const browser = await launcher.launch()
-  const page = await browser.newPage()
-  return { page, browser }
+export type TestCases = TestCasesPlaywright | TestCasesPuppeteer
+
+export const wrap = (ava: any) => (cases: TestCases) => (
+  title: string,
+  testFn: any
+) => {
+  const driverName = cases.split(':')[0] as DriverShortNames
+  const browserName = cases.split(':')[1] as Browsers
+
+  testFn.title = (title = '', cases = '') =>
+    `${driverName}(${browserName}): ${title}`
+
+  ava(title, testFn, driver(driverName, browserName))
 }
