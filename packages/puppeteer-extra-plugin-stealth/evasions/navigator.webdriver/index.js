dc83227a66923fdfe5a49e98590292d108abee3e
Niek van der Maas <mail@niekvandermaas.nl>
2020-10-29 20:57:06 +0100

    Check if blink features already set, document how to remove infobar

--- packages/puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index.js
+++ packages/puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index.js
@@ -23,7 +23,16 @@ class Plugin extends PuppeteerExtraPlugin {
   }
 
   // Post Chrome 88.0.4291.0
+  // Note: this will add an infobar to Chrome with a warning that an unsupported flag is set
+  // To remove this bar on Linux, run: mkdir -p /etc/opt/chrome/policies/managed && echo '{ "CommandLineFlagSecurityWarningsEnabled": false }' > /etc/opt/chrome/policies/managed/managed_policies.json
   async beforeLaunch(options) {
+    // If disable-blink-features is already passed, append the AutomationControlled switch
+    options.args.forEach(e => {
+      if (e.startsWith('--disable-blink-features=')) {
+        e += ',AutomationControlled'
+        return // eslint-disable-line
+      }
+    })
     options.args.push(`--disable-blink-features=AutomationControlled`)
   }
 }
