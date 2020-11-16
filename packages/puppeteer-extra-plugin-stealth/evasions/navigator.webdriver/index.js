6ffbbec012850f26d6b756b4b24d252da77284d6
Markus <endzeit@gmail.com>
2020-11-16 01:28:03 +0100

    fix(plugin-stealth): Don't assume options.arg exists already

--- packages/puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index.js
+++ packages/puppeteer-extra-plugin-stealth/evasions/navigator.webdriver/index.js
@@ -26,6 +26,7 @@ class Plugin extends PuppeteerExtraPlugin {
   // Note: this will add an infobar to Chrome with a warning that an unsupported flag is set
   // To remove this bar on Linux, run: mkdir -p /etc/opt/chrome/policies/managed && echo '{ "CommandLineFlagSecurityWarningsEnabled": false }' > /etc/opt/chrome/policies/managed/managed_policies.json
   async beforeLaunch(options) {
+    options.args = options.args || []
     // If disable-blink-features is already passed, append the AutomationControlled switch
     options.args.forEach(e => {
       if (e.startsWith('--disable-blink-features=')) {
