bbe1dee639f4216bb70d41c009cc5a7dab0dc9af
Niek van der Maas <mail@niekvandermaas.nl>
2021-01-05 15:17:30 +0100

    Added test for inconsistencies between page and service workers

--- packages/puppeteer-extra-plugin-stealth/test/cat-and-mouse.test.js
+++ packages/puppeteer-extra-plugin-stealth/test/cat-and-mouse.test.js
@@ -2,6 +2,9 @@ const test = require('ava')
 
 const { vanillaPuppeteer, addExtra } = require('./util')
 const Plugin = require('..')
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 // Fix CI issues with old versions
 const isOldPuppeteerVersion = () => {
@@ -10,15 +13,61 @@ const isOldPuppeteerVersion = () => {
   return isOld
 }
 
+// Create a simple HTTP server. Service Workers cannot be served from file:// URIs
+const httpServer = async () => {
+  const server = await http
+    .createServer((req, res) => {
+      let contents, type
+
+      if (req.url === '/sw.js') {
+        contents = fs.readFileSync(path.join(__dirname, './fixtures/sw.js'))
+        type = 'application/javascript'
+      } else {
+        contents = fs.readFileSync(
+          path.join(__dirname, './fixtures/dummy-with-service-worker.html')
+        )
+        type = 'text/html'
+      }
+
+      res.setHeader('Content-Type', type)
+      res.writeHead(200)
+      res.end(contents)
+    })
+    .listen(0) // random free port
+
+  return `http://127.0.0.1:${server.address().port}/`
+}
+
+let browser, page, worker
+
+test.before(async t => {
+  const address = await httpServer()
+  console.log(`Server is running on port ${address}`)
+
+  browser = await addExtra(vanillaPuppeteer)
+    .use(Plugin())
+    .launch({ headless: true })
+  page = await browser.newPage()
+
+  worker = new Promise(resolve => {
+    browser.on('targetcreated', async target => {
+      if (target.type() === 'service_worker') {
+        resolve(target.worker())
+      }
+    })
+  })
+
+  await page.goto(address)
+})
+
+test.after(async t => {
+  await browser.close()
+})
+
 /* global HTMLIFrameElement */
 /* global Notification */
 test('stealth: will pass Paul Irish', async t => {
-  const browser = await addExtra(vanillaPuppeteer)
-    .use(Plugin())
-    .launch({ headless: true })
-  const page = await browser.newPage()
   const detectionResults = await page.evaluate(detectHeadless)
-  await browser.close()
 
   if (isOldPuppeteerVersion()) {
     t.true(true)
@@ -30,6 +79,13 @@ test('stealth: will pass Paul Irish', async t => {
     console.log(detectionResults)
   }
   t.false(wasHeadlessDetected)
+})
+
+test('stealth: inconsistencies between page and worker', async t => {
+  const pageFP = await page.evaluate(detectFingerprint)
+  const workerFP = await (await worker).evaluate(detectFingerprint)
+
+  t.is(pageFP, workerFP)
 })
 
 async function detectHeadless() {
@@ -149,6 +205,37 @@ async function detectHeadless() {
     console.debug(spooky)
     return gotYou > 1
   })
+
+  return results
+}
+
+/* global OffscreenCanvas */
+function detectFingerprint() {
+  const results = {}
+
+  const props = [
+    'userAgent',
+    'language',
+    'hardwareConcurrency',
+    'deviceMemory',
+    'languages',
+    'platform'
+  ]
+  props.forEach(el => {
+    results[el] = navigator[el].toString()
+  })
+
+  const canvasOffscreenWebgl = new OffscreenCanvas(256, 256)
+  const contextWebgl = canvasOffscreenWebgl.getContext('webgl')
+  const rendererInfo = contextWebgl.getExtension('WEBGL_debug_renderer_info')
+  results.webglVendor = contextWebgl.getParameter(
+    rendererInfo.UNMASKED_VENDOR_WEBGL
+  )
+  results.webglRenderer = contextWebgl.getParameter(
+    rendererInfo.UNMASKED_RENDERER_WEBGL
+  )
+
+  results.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
   return results
 }
