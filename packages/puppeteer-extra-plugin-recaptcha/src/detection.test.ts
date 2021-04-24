a32d5c7ce36cb4aff7823428763d4ae0e44c7979
Markus <endzeit@gmail.com>
2021-04-24 12:57:45 +0200

    chore(plugin-recaptcha): Add more detection tests

---
+++ packages/puppeteer-extra-plugin-recaptcha/src/detection.test.ts
@@ -0,0 +1,100 @@
+import test from 'ava'
+
+import RecaptchaPlugin from './index'
+
+import { addExtra } from 'puppeteer-extra'
+
+const PUPPETEER_ARGS = ['--no-sandbox', '--disable-setuid-sandbox']
+
+const getBrowser = async (url = '') => {
+  const puppeteer = addExtra(require('puppeteer'))
+  const recaptchaPlugin = RecaptchaPlugin()
+  puppeteer.use(recaptchaPlugin)
+  const browser = await puppeteer.launch({
+    args: PUPPETEER_ARGS,
+    headless: true
+  })
+  const page = await browser.newPage()
+  await page.goto(url, { waitUntil: 'networkidle0' })
+  return { browser, page }
+}
+
+test('will correctly detect v2-checkbox-auto.html', async t => {
+  const url =
+    'https://berstend.github.io/static/recaptcha/v2-checkbox-auto.html'
+  const { browser, page } = await getBrowser(url)
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'recaptcha')
+  t.is(c.callback, undefined)
+  t.is(c.hasResponseElement, true)
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+  t.is(c.widgetId, 0)
+  t.not(c.display, undefined)
+
+  await browser.close()
+})
+
+test('will correctly detect v2-checkbox-auto-recaptchadotnet.html', async t => {
+  const url =
+    'https://berstend.github.io/static/recaptcha/v2-checkbox-auto-recaptchadotnet.html'
+  const { browser, page } = await getBrowser(url)
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'recaptcha')
+  t.is(c.callback, undefined)
+  t.is(c.hasResponseElement, true)
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+  t.is(c.widgetId, 0)
+  t.not(c.display, undefined)
+
+  await browser.close()
+})
+
+test('will correctly detect enterprise-checkbox-auto.html', async t => {
+  const url =
+    'https://berstend.github.io/static/recaptcha/enterprise-checkbox-auto.html'
+  const { browser, page } = await getBrowser(url)
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'recaptcha')
+  t.is(c.callback, undefined)
+  t.is(c.hasResponseElement, true)
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+  t.is(c.widgetId, 0)
+  t.not(c.display, undefined)
+
+  await browser.close()
+})
+
+test('will correctly detect enterprise-checkbox-auto-recaptchadotnet.html', async t => {
+  const url =
+    'https://berstend.github.io/static/recaptcha/enterprise-checkbox-auto-recaptchadotnet.html'
+  const { browser, page } = await getBrowser(url)
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'recaptcha')
+  t.is(c.callback, undefined)
+  t.is(c.hasResponseElement, true)
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+  t.is(c.widgetId, 0)
+  t.not(c.display, undefined)
+
+  await browser.close()
+})
