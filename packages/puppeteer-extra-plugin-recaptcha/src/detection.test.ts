4f07aca377b46ffdcb2c857d00a046b585b62d15
Markus <endzeit@gmail.com>
2021-04-25 15:22:58 +0200

    feat(plugin-recaptcha): Support more recaptcha domain combos

--- packages/puppeteer-extra-plugin-recaptcha/src/detection.test.ts
+++ packages/puppeteer-extra-plugin-recaptcha/src/detection.test.ts
@@ -40,6 +40,26 @@ test('will correctly detect v2-checkbox-auto.html', async t => {
   await browser.close()
 })
 
+test('will correctly detect v2-checkbox-auto-nowww.html', async t => {
+  const url =
+    'https://berstend.github.io/static/recaptcha/v2-checkbox-auto-nowww.html'
+  const { browser, page } = await getBrowser(url)
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'recaptcha')
+  t.is(c.callback, undefined)
+  t.is(c.hasResponseElement, true)
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+  t.is(c.widgetId, 0)
+  t.not(c.display, undefined)
+
+  await browser.close()
+})
+
 test('will correctly detect v2-checkbox-auto-recaptchadotnet.html', async t => {
   const url =
     'https://berstend.github.io/static/recaptcha/v2-checkbox-auto-recaptchadotnet.html'
