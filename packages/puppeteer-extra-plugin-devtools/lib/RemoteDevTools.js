1f8d508bebe4842ca38bc116f1923ef4cd3abf01
abowcut <alexbowcut@gmail.com>
2021-09-28 13:56:10 -0600

    swapped to console logging instead of debug log

--- packages/puppeteer-extra-plugin-devtools/lib/RemoteDevTools.js
+++ packages/puppeteer-extra-plugin-devtools/lib/RemoteDevTools.js
@@ -265,7 +265,7 @@ class DevToolsTunnel extends DevToolsCommon {
     })
 
     tunnel.on('error', (err) => {
-      debug('tunnel errored', err)
+      console.log('tunnel error', err);
     })
 
     debug('tunnel:created', tunnel.url)
