cf9713773f6f7a957162007bc50e0ca9c60b362d
urielch <uriel.chemouni@gmail.com>
2022-06-06 09:42:21 +0300

    remove ava config files

--- packages/puppeteer-extra/ava.config-ts.js
+++
@@ -1,9 +0,0 @@
-export default {
-  compileEnhancements: false,
-  environmentVariables: {
-    TS_NODE_COMPILER_OPTIONS: '{"module":"commonjs"}'
-  },
-  files: ['test/*.ts'],
-  extensions: ['ts'],
-  require: ['ts-node/register']
-}
