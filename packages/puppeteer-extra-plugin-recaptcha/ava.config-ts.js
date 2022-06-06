d71830a651395f83651483ff3ca1fe1eea4fb36b
urielch <uriel.chemouni@gmail.com>
2022-06-06 16:53:41 +0300

    drop all ava config files

--- packages/puppeteer-extra-plugin-recaptcha/ava.config-ts.js
+++
@@ -1,9 +0,0 @@
-export default {
-  compileEnhancements: false,
-  environmentVariables: {
-    TS_NODE_COMPILER_OPTIONS: '{"module":"commonjs"}'
-  },
-  files: ['src/**.test.ts'],
-  extensions: ['ts'],
-  require: ['ts-node/register']
-}
--- packages/puppeteer-extra-plugin-recaptcha/ava.config.js
+++
@@ -1,3 +0,0 @@
-export default {
-  files: ['dist/*.test.js']
-}
