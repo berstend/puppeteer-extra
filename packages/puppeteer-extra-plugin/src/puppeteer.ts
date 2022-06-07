f63aa0c8a3a13429b2a55e9365ffcc8804a7b5d1
UrielCh <uriel.chemouni@gmail.com>
2022-06-07 05:24:21 +0300

    Revert "trey fix build error"
    This reverts commit ce2ffc5070339c341f163133e0f2c9f93176375c.

--- packages/puppeteer-extra-plugin/src/puppeteer.ts
+++ packages/puppeteer-extra-plugin/src/puppeteer.ts
@@ -1,4 +1,3 @@
-/// <reference path="../../puppeteer-legacy.d.ts" />
 // A wildcard import would result in a `require("puppeteer")` statement
 // at the top of the transpiled js file, not what we want. :-/
 // "import type" is a solution here but requires TS >= v3.8 which we don't want to require yet as a minimum.
