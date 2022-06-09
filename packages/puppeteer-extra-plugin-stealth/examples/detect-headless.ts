efb9ba1152220a0d0687be8e2614725a34261502
UrielCh <uriel.chemouni@gmail.com>
2022-06-09 12:19:51 +0300

    update tests

--- packages/puppeteer-extra-plugin-stealth/examples/detect-headless.ts
+++ packages/puppeteer-extra-plugin-stealth/examples/detect-headless.ts
@@ -1,13 +1,18 @@
-'use strict'
-
 // taken from: https://github.com/paulirish/headless-cat-n-mouse/blob/master/detect-headless.js
 // initial detects from @antoinevastel
+
+// import { JSHandle } from "puppeteer";
+
 //   http://antoinevastel.github.io/bot%20detection/2018/01/17/detect-chrome-headless-v2.html
+declare var window: any;
 
-module.exports = async function() {
-  const results = {}
+/**
+ * will be evaluated in the browser context
+ */
+export default async function() {
+  const results: {[key: string]: boolean} = {}
 
-  async function test(name, fn) {
+  async function test(name: string, fn: () => boolean | Promise<boolean>): Promise<void> {
     const detectionPassed = await fn()
     if (detectionPassed) {
       console.log(`WARNING: Chrome headless detected via ${name}`)
@@ -17,32 +22,31 @@ module.exports = async function() {
     results[name] = detectionPassed
   }
 
-  await test('userAgent', _ => {
+  await test('userAgent', () => {
     return /HeadlessChrome/.test(window.navigator.userAgent)
   })
 
   // Detects the --enable-automation || --headless flags
   // Will return true in headful if --enable-automation is provided
-  await test('navigator.webdriver present', _ => {
+  await test('navigator.webdriver present', () => {
     return 'webdriver' in navigator
   })
 
-  await test('window.chrome missing', _ => {
+  await test('window.chrome missing', () => {
     return /Chrome/.test(window.navigator.userAgent) && !window.chrome
   })
 
-  await test('permissions API', async _ => {
+  await test('permissions API', async () => {
     const permissionStatus = await navigator.permissions.query({
       name: 'notifications'
     })
-    // eslint-disable-next-line
     return (
-      Notification.permission === 'denied' && // eslint-disable-line no-undef
+      Notification.permission === 'denied' &&
       permissionStatus.state === 'prompt'
     )
   })
 
-  await test('permissions API overriden', _ => {
+  await test('permissions API overriden', () => {
     const permissions = window.navigator.permissions
     if (permissions.query.toString() !== 'function query() { [native code] }')
       return true
@@ -58,17 +62,19 @@ module.exports = async function() {
     )
       return true
     if (permissions.hasOwnProperty('query')) return true // eslint-disable-line no-prototype-builtins
+    return false
   })
 
-  await test('navigator.plugins empty', _ => {
+  await test('navigator.plugins empty', () => {
     return navigator.plugins.length === 0
   })
 
-  await test('navigator.languages blank', _ => {
-    return navigator.languages === ''
+  await test('navigator.languages blank', () => {
+    // console.log( (navigator.languages as unknown as JSHandle).toString() )
+    return (navigator as any).languages === ''
   })
 
-  await test('iFrame for fresh window object', _ => {
+  await test('iFrame for fresh window object', () => {
     // evaluateOnNewDocument scripts don't apply within [srcdoc] (or [sandbox]) iframes
     // https://github.com/GoogleChrome/puppeteer/issues/1106#issuecomment-359313898
     const iframe = document.createElement('iframe')
@@ -77,19 +83,18 @@ module.exports = async function() {
 
     // Here we would need to rerun all tests with `iframe.contentWindow` as `window`
     // Example:
-    return iframe.contentWindow.navigator.plugins.length === 0
+    return iframe.contentWindow!.navigator.plugins.length === 0
   })
 
   // This detects that a devtools protocol agent is attached.
   // So it will also pass true in headful Chrome if the devtools window is attached
-  await test('toString', _ => {
+  await test('toString', () => {
     let gotYou = 0
     const spooky = /./
     spooky.toString = function() {
       gotYou++
       return 'spooky'
     }
-    console.debug(spooky)
     return gotYou > 1
   })
 
