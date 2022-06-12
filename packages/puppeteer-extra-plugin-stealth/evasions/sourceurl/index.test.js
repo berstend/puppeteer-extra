36302707e27dd681b9a52062c2204bec497d0e5a
UrielCh <uriel.chemouni@gmail.com>
2022-06-12 20:41:34 +0300

    restore sourceurl test

--- packages/puppeteer-extra-plugin-stealth/evasions/sourceurl/index.test.js
+++ packages/puppeteer-extra-plugin-stealth/evasions/sourceurl/index.test.js
@@ -24,7 +24,7 @@ test('vanilla: sourceurl is leaking', async t => {
   // Error: Test Error at test (file:///home/runner/work/puppeteer-extra-ts/puppeteer-extra-ts/packages/puppeteer-extra-plugin-stealth/evasions/sourceurl/_fixtures/test.html:13:21)
   // at HTMLDocument.querySelector (file:///home/runner/work/puppeteer-extra-ts/puppeteer-extra-ts/packages/puppeteer-extra-plugin-stealth/evasions/sourceurl/_fixtures/test.html:25:13)
   // at pptr://__puppeteer_evaluation_script__:1:17'
-  t.regex(result, /^_FAIL/)
+  t.regex(result, /^FAIL/)
   const result2 = await page.evaluate(() => {
     try {
       Function.prototype.toString.apply({})
