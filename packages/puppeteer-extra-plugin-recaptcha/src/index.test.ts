a4bd5e71aa4332076a170229ebd40598a91a6f6b
berstend <github@berstend.com>
2022-06-20 16:11:30 +0200

    chore(plugin-recaptcha): Add hcaptcha active challenge test

--- packages/puppeteer-extra-plugin-recaptcha/src/index.test.ts
+++ packages/puppeteer-extra-plugin-recaptcha/src/index.test.ts
@@ -63,6 +63,39 @@ test('will detect hCAPTCHAs', async t => {
   await browser.close()
 })
 
+test('will detect active hCAPTCHA challenges', async t => {
+  const puppeteer = addExtra(require('puppeteer'))
+  const recaptchaPlugin = RecaptchaPlugin()
+  puppeteer.use(recaptchaPlugin)
+
+  const browser = await puppeteer.launch({
+    args: PUPPETEER_ARGS,
+    headless: true
+  })
+  const page = await browser.newPage()
+
+  const url = 'https://democaptcha.com/demo-form-eng/hcaptcha.html'
+  await page.goto(url, { waitUntil: 'networkidle0' })
+  await page.evaluate(() => (window as any).hcaptcha.execute()) // trigger challenge popup
+  await page.waitForTimeout(2 * 1000)
+  await page.evaluate(() =>
+    document
+      .querySelector(`[data-hcaptcha-widget-id]:not([src*='invisible'])`)
+      .remove()
+  ) // remove regular checkbox so we definitely test against the popup
+
+  const { captchas, error } = await (page as any).findRecaptchas()
+  t.is(error, null)
+  t.is(captchas.length, 1)
+
+  const c = captchas[0]
+  t.is(c._vendor, 'hcaptcha')
+  t.is(c.url, url)
+  t.true(c.sitekey && c.sitekey.length > 5)
+
+  await browser.close()
+})
+
 test('will not throw when no captchas are found', async t => {
   const puppeteer = addExtra(require('puppeteer'))
   const recaptchaPlugin = RecaptchaPlugin()
